efe75b8ac729624b36b7e84250468612f307ffc2
jumpcutfindo <danielhoe97@gmail.com>
2023-10-27 21:36:00 +0800

    Add photos page and photos

---
+++ app/photography/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import photosJson from "./api/photos.json";
+import Photo from "../types/photo";
+import { Dialog } from "@headlessui/react";
+
+interface PhotoModalProps {
+  isOpen: boolean;
+  setIsOpen: (arg: boolean) => void;
+  photo: Photo | undefined;
+}
+
+function PhotoModal(props: PhotoModalProps) {
+  const { photo, isOpen, setIsOpen } = props;
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      className="fixed inset-0 z-10 overflow-y-auto flex justify-center items-center bg-black/60"
+    >
+      <Dialog.Panel className="max-w-screen-xl">
+        {photo && (
+          <Image
+            src={photo.image}
+            width="0"
+            height="0"
+            className="w-full max-h-screen"
+            alt={photo.description}
+          />
+        )}
+      </Dialog.Panel>
+    </Dialog>
+  );
+}
+
+export default function Photography() {
+  const photos = photosJson as Photo[];
+
+  const [currentPhoto, setCurrentPhoto] = useState<Photo | undefined>(
+    undefined
+  );
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const onOpenPhoto = (photo: Photo) => {
+    setCurrentPhoto(photo);
+    setModalOpen(true);
+  };
+
+  return (
+    <main className="flex lg:py-24 md:py-12 py-6 w-screen">
+      <div className="flex flex-col flex-grow">
+        <div className="flex md:mx-12 mx-6 mb-4">
+          <a
+            className="text-sm font-semibold hover:cursor-pointer group transition-all"
+            href="/"
+          >
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              className="mx-2 group-hover:-translate-x-1/2 group-hover:text-blue-400 transition-all"
+            />
+
+            <span className="group-hover:text-blue-400 transition-all">
+              Daniel Hoe
+            </span>
+          </a>
+        </div>
+
+        <div className="flex flex-col space-y-8">
+          <div>
+            <h1 className="text-5xl font-semibold text-blue-500 md:mx-12 mx-6">
+              Photography
+            </h1>
+          </div>
+          <div className="columns-1 md:columns-2 lg:columns-3 md:mx-12 mx-6 gap-8 md:space-y-8 space-y-4">
+            {photos.map((photo, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col space-y-2 w-full break-inside-avoid-column hover:cursor-pointer group"
+                  onClick={() => onOpenPhoto(photo)}
+                >
+                  <Image
+                    src={photo.image}
+                    width="0"
+                    height="0"
+                    sizes="100vw"
+                    className="w-full"
+                    alt={photo.description}
+                  />
+                  <p className="text-xs italic text-gray-400 group-hover:text-white transition">
+                    {photo.location}, {photo.country} ({photo.year})
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+          <PhotoModal
+            isOpen={isModalOpen}
+            setIsOpen={setModalOpen}
+            photo={currentPhoto}
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
+function setState(arg0: boolean): [any, any] {
+  throw new Error("Function not implemented.");
+}
