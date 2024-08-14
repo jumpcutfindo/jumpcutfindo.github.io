10f53cd615ee981e0dd9ec7955c88ae6f05b74cd
jumpcutfindo <danielhoe97@gmail.com>
2024-08-14 17:34:25 +0800

    Remove landing page

--- app/(intentional)/intentional/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Intentional() {
-  return (
-    <main className="flex lg:py-24 md:py-12 py-6 w-screen">
-      <div className="flex flex-col flex-grow">Hello world!</div>
-    </main>
-  );
-}
