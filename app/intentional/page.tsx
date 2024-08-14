4b4c529b11966df27dec5fb4e692dcd07aeecee1
jumpcutfindo <danielhoe97@gmail.com>
2024-08-14 12:26:12 +0800

    Add basic page for `intentional`

---
+++ app/intentional/page.tsx
@@ -0,0 +1,7 @@
+export default function Intentional() {
+  return (
+    <main className="flex lg:py-24 md:py-12 py-6 w-screen">
+      <div className="flex flex-col flex-grow">Hello world!</div>
+    </main>
+  );
+}
