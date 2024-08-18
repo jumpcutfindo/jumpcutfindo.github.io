c5750feb46b4fcb618a67fe3eae37348981e03ae
jumpcutfindo <danielhoe97@gmail.com>
2024-08-18 11:04:17 +0800

    Update layout to reduce skeleton

--- app/(intentional)/layout.tsx
+++ app/(intentional)/layout.tsx
@@ -32,11 +32,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`flex justify-center w-screen ${inter.className}`}>
-        <div className="flex bg-black w-screen min-h-screen justify-center">
-          {children}
-        </div>
-      </body>
+      <body className={`bg-black ${inter.className}`}>{children}</body>
     </html>
   );
 }
