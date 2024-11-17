c854de04970e85b0771a00a8689ea4000066a8ae
jumpcutfindo <danielhoe97@gmail.com>
2024-11-17 18:08:40 +0800

    Create types for different cards

--- app/(language)/language/mandarin/types/card.ts
+++ app/(language)/language/mandarin/types/card.ts
@@ -1,4 +1,26 @@
+import { MandarinDefinition } from "./mandarin";
+
+export enum CardType {
+  FillBlank = "fill-in-the-blank",
+  MatchDefinition = "match-definition",
+  MatchPinyin = "match-pinyin",
+}
+
 export interface MandarinCardProps {
   onCorrect: () => void;
   onIncorrect: () => void;
+}
+
+export interface FillBlankCardProps extends MandarinCardProps {
+  answer: MandarinDefinition;
+  options: MandarinDefinition[];
+  sentence: string;
+}
+
+export interface MatchDefinitionCardProps extends MandarinCardProps {
+  options: MandarinDefinition[];
+}
+
+export interface MatchPinyinCardProps extends MandarinCardProps {
+  options: MandarinDefinition[];
 }
