34f2fb689d0733e53d58a319c489efdd8d24feab
jumpcutfindo <danielhoe97@gmail.com>
2024-11-17 18:01:23 +0800

    Add import for Chinese JSON

--- app/(language)/language/mandarin/page.tsx
+++ app/(language)/language/mandarin/page.tsx
@@ -1,12 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
 
+import chineseJson from "./api/chinese.json";
+import { MandarinDefinition } from "./types/mandarin";
+
 export const metadata: Metadata = {
   title: "Mandarin Quiz",
   description: "The mandarin quiz!",
 };
 
 export default function MandarinQuiz() {
+  const chinese = chineseJson as MandarinDefinition[];
+
   return (
     <div className="flex flex-col h-screen w-screen justify-center items-center">
       <div className="flex flex-col items-center w-[480px] flex-1 bg-slate-900">
