e63885ca5e7c4400edf0d3f108c126267257d2ad
jumpcutfindo <danielhoe97@gmail.com>
2024-11-18 02:09:12 +0800

    Implement quiz states

--- app/(language)/language/mandarin/page.tsx
+++ app/(language)/language/mandarin/page.tsx
@@ -7,19 +7,36 @@ import { MandarinDefinition } from "./types/mandarin";
 import FillBlankMandarinCard from "./cards/fill-in-the-blank";
 import { generateFillBlank } from "./utils/generate-fill-blank";
 import { faLanguage } from "@fortawesome/free-solid-svg-icons";
+import { QuizState } from "./types/quiz";
+import { useEffect, useState } from "react";
+import { FillBlankCard } from "./types/card";
 
 export default function MandarinQuiz() {
   const chinese = chineseJson as MandarinDefinition[];
 
-  const fillBlankExample = generateFillBlank(chinese, 4);
+  const [quizState, setQuizState] = useState(QuizState.Question);
+  const [fillBlankExample, setFillBlankExample] =
+    useState<FillBlankCard | null>();
 
   const onCorrect = () => {
+    setQuizState(QuizState.Review);
     console.log("Answer was correct");
   };
 
   const onIncorrect = () => {
+    setQuizState(QuizState.Review);
     console.log("Answer was incorrect");
   };
+
+  const onNext = () => {
+    setQuizState(QuizState.Question);
+  };
+
+  useEffect(() => {
+    if (quizState === QuizState.Question) {
+      setFillBlankExample(generateFillBlank(chinese, 4));
+    }
+  }, [quizState]);
 
   return (
     <div className="flex flex-col h-screen w-screen justify-center items-center">
@@ -28,16 +45,18 @@ export default function MandarinQuiz() {
           <FontAwesomeIcon icon={faLanguage} size="xl" />
           <h1>中文测验</h1>
         </div>
-        <div className="w-full flex flex-col flex-1">
-          <FillBlankMandarinCard
-            answer={fillBlankExample.answer}
-            options={fillBlankExample.options}
-            example={fillBlankExample.example}
-            blankedSentence={fillBlankExample.blankedSentence}
-            onCorrect={onCorrect}
-            onIncorrect={onIncorrect}
-          />
-        </div>
+        {fillBlankExample && (
+          <div className="w-full flex flex-col flex-1">
+            <FillBlankMandarinCard
+              answer={fillBlankExample.answer}
+              options={fillBlankExample.options}
+              example={fillBlankExample.example}
+              blankedSentence={fillBlankExample.blankedSentence}
+              onCorrect={onCorrect}
+              onIncorrect={onIncorrect}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
