600f18114b5a2eefbe3366ffd85c60f8e54f042e
jumpcutfindo <danielhoe97@gmail.com>
2024-11-17 22:35:11 +0800

    Create card wrapper for layout

---
+++ app/(language)/language/mandarin/cards/card-wrapper.tsx
@@ -0,0 +1,20 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface CardWrapperProps {
+  icon: IconProp;
+  title: string;
+  children: React.ReactNode;
+}
+
+export function CardWrapper({ icon, title, children }: CardWrapperProps) {
+  return (
+    <div className="flex flex-col rounded-lg m-2 px-2 py-2 space-y-4">
+      <div className="w-full flex flex-row items-start space-x-2">
+        <FontAwesomeIcon icon={icon} size="sm" className="my-auto" />
+        <h1 className="my-auto text-sm font-bold">{title.toUpperCase()}</h1>
+      </div>
+      <div className="w-full flex flex-col flex-1 space-y-4">{children}</div>
+    </div>
+  );
+}
