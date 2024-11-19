3813ab6fa67655f463d0ae7f5941a3ae53f3bf5e
jumpcutfindo <danielhoe97@gmail.com>
2024-11-19 16:50:04 +0800

    Implement shuffling util

---
+++ app/(language)/language/quiz/utils/shuffle.ts
@@ -0,0 +1,17 @@
+export function shuffle(array: any[]) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    // Pick a remaining element
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
