b5cd7628c99c57ff060beed4763842ce37381d68
jumpcutfindo <danielhoe97@gmail.com>
2024-11-20 18:51:54 +0800

    Add logic for ending round

--- app/(language)/language/mandarin/utils/generate-card-type.ts
+++ app/(language)/language/mandarin/utils/generate-card-type.ts
@@ -3,9 +3,9 @@ import { MandarinCardType } from "../types/card";
 export function generateCardType(): MandarinCardType {
   const p = Math.random();
 
-  if (p < 0.6) {
+  if (p < 1.0) {
     // Weigh fill blank more heavily
-    return MandarinCardType.FillBlank;
+    return MandarinCardType.MatchPinyin;
   } else if (p < 0.8) {
     return MandarinCardType.MatchPinyin;
   } else {
