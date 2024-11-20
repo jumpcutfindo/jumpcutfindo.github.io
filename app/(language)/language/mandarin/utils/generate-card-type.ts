e765be676a0a8735b4322d5dbebd4b4f99caccf7
jumpcutfindo <danielhoe97@gmail.com>
2024-11-20 19:16:53 +0800

    Fix generation probability

--- app/(language)/language/mandarin/utils/generate-card-type.ts
+++ app/(language)/language/mandarin/utils/generate-card-type.ts
@@ -3,9 +3,9 @@ import { MandarinCardType } from "../types/card";
 export function generateCardType(): MandarinCardType {
   const p = Math.random();
 
-  if (p < 1.0) {
+  if (p < 0.6) {
     // Weigh fill blank more heavily
-    return MandarinCardType.MatchPinyin;
+    return MandarinCardType.FillBlank;
   } else if (p < 0.8) {
     return MandarinCardType.MatchPinyin;
   } else {
