cafeebb43871df3955a6dfd9201d9b593fc419d7
jumpcutfindo <danielhoe97@gmail.com>
2024-11-22 21:01:25 +0800

    Implement reference list

--- app/(language)/mandarin/reference/page.tsx
+++ app/(language)/mandarin/reference/page.tsx
@@ -2,13 +2,19 @@
 
 import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
 import {
+  LanguageBody,
   LanguageHeader,
   LanguageHeaderContent,
   LanguageLayout,
 } from "../../language-layout";
 import { MandarinMetadataComponent } from "../metadata";
+import { MandarinDefinition } from "../api/mandarin";
+import chineseJson from "../api/chinese.json";
+import { MandarinReferenceList } from "./reference-list";
 
 export default function MandarinReference() {
+  const chinese = chineseJson as MandarinDefinition[];
+
   return (
     <LanguageLayout>
       <LanguageHeader icon={faBookBookmark} title="词语资料库">
@@ -16,6 +22,9 @@ export default function MandarinReference() {
           <MandarinMetadataComponent />
         </LanguageHeaderContent>
       </LanguageHeader>
+      <LanguageBody>
+        <MandarinReferenceList chinese={chinese} />
+      </LanguageBody>
     </LanguageLayout>
   );
 }
---
+++ app/(language)/mandarin/reference/reference-list.tsx
@@ -0,0 +1,53 @@
+import { Virtuoso } from "react-virtuoso";
+import { MandarinDefinition, MandarinWordType } from "../api/mandarin";
+
+const WORD_TYPE_CLASS_MAP: Record<MandarinWordType, string> = {
+  noun: "text-sky-400",
+  verb: "text-emerald-400",
+  idiom: "text-rose-400",
+  adjective: "text-amber-400",
+  adverb: "text-pink-400",
+  conjunction: "text-pink-400",
+};
+
+interface MandarinReferenceListItemProps {
+  definition: MandarinDefinition;
+}
+
+export function MandarinReferenceListItem({
+  definition,
+}: MandarinReferenceListItemProps) {
+  return (
+    <div className="w-full border rounded-lg p-4 my-2">
+      <div className="flex flex-row items-center space-x-2">
+        <span className="text-2xl">{definition.word}</span>
+        <span className="text-lg opacity-80">{definition.pinyin}</span>
+      </div>
+      <div className="flex flex-row items-baseline space-x-2">
+        <p className="space-x-2">
+          <span
+            className={`text-sm font-bold ${WORD_TYPE_CLASS_MAP[definition.wordType]}`}
+          >
+            {definition.wordType.toUpperCase()}
+          </span>
+          <span>{definition.definition}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export interface MandarinReferenceListProps {
+  chinese: MandarinDefinition[];
+}
+
+export function MandarinReferenceList({ chinese }: MandarinReferenceListProps) {
+  return (
+    <Virtuoso
+      totalCount={chinese.length}
+      itemContent={(index) => (
+        <MandarinReferenceListItem definition={chinese[index]} />
+      )}
+    />
+  );
+}
