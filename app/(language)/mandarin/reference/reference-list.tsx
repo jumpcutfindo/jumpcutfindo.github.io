163af0426f1fd37bd682652ea37463450e5629a1
jumpcutfindo <danielhoe97@gmail.com>
2024-11-22 22:59:39 +0800

    Implement reference list item

--- app/(language)/mandarin/reference/reference-list.tsx
+++ app/(language)/mandarin/reference/reference-list.tsx
@@ -18,20 +18,22 @@ export function MandarinReferenceListItem({
   definition,
 }: MandarinReferenceListItemProps) {
   return (
-    <div className="w-full border rounded-lg p-4 my-2">
-      <div className="flex flex-row items-center space-x-2">
-        <span className="text-2xl">{definition.word}</span>
-        <span className="text-lg opacity-80">{definition.pinyin}</span>
-      </div>
-      <div className="flex flex-row items-baseline space-x-2">
-        <p className="space-x-2">
-          <span
-            className={`text-sm font-bold ${WORD_TYPE_CLASS_MAP[definition.wordType]}`}
-          >
-            {definition.wordType.toUpperCase()}
-          </span>
-          <span>{definition.definition}</span>
-        </p>
+    <div className="w-full">
+      <div className="flex flex-col border rounded-lg p-4 mb-2 mx-2">
+        <div className="flex flex-row items-center space-x-2">
+          <span className="text-2xl">{definition.word}</span>
+          <span className="text-lg opacity-80">{definition.pinyin}</span>
+        </div>
+        <div className="flex flex-row items-baseline space-x-2">
+          <p className="space-x-2">
+            <span
+              className={`text-sm font-bold ${WORD_TYPE_CLASS_MAP[definition.wordType]}`}
+            >
+              {definition.wordType.toUpperCase()}
+            </span>
+            <span>{definition.definition}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
