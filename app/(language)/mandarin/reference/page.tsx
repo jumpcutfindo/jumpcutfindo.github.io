3880a830f775cc21bcb4642da827140fbc18f864
jumpcutfindo <danielhoe97@gmail.com>
2024-11-22 16:03:41 +0800

    Add layout for reference page

--- app/(language)/mandarin/reference/page.tsx
+++ app/(language)/mandarin/reference/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  LanguageHeader,
+  LanguageHeaderContent,
+  LanguageLayout,
+} from "../../language-layout";
+import { MandarinMetadataComponent } from "../metadata";
+
 export default function MandarinReference() {
-  return <div>Reference</div>;
+  return (
+    <LanguageLayout>
+      <LanguageHeader icon={faBookBookmark} title="词语资料库">
+        <LanguageHeaderContent>
+          <MandarinMetadataComponent />
+        </LanguageHeaderContent>
+      </LanguageHeader>
+    </LanguageLayout>
+  );
 }
