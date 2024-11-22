6030bc32c847bd535a261728c5afeaba2f42ec20
jumpcutfindo <danielhoe97@gmail.com>
2024-11-22 09:00:43 +0800

    Add prettier package and config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+};
