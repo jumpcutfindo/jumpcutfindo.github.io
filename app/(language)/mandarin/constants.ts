24b4be25615d32fe800b9948630ea5d66d8b0d0f
jumpcutfindo <danielhoe97@gmail.com>
2024-11-28 21:34:28 +0800

    Add definitions for menu items

--- app/(language)/mandarin/constants.ts
+++ app/(language)/mandarin/constants.ts
@@ -1,4 +1,19 @@
+import { faBookBookmark, faLanguage } from "@fortawesome/free-solid-svg-icons";
+
 import { MandarinWordType } from "./api/mandarin";
+
+export const MANDARIN_MENU_ITEMS = [
+  {
+    title: "词语资料库",
+    href: "/mandarin/reference",
+    icon: faBookBookmark,
+  },
+  {
+    title: "中文测验",
+    href: "/mandarin/quiz",
+    icon: faLanguage,
+  },
+];
 
 export const WORD_TYPE_CLASS_MAP: Record<MandarinWordType, string> = {
   noun: "text-sky-400",
