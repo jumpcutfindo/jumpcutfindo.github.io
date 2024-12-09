167b0193324c6c4d227797836225c09fd3ea1dbe
jumpcutfindo <danielhoe97@gmail.com>
2024-12-09 21:29:53 +0800

    Add stats for match pinyin

--- app/(language)/mandarin/quiz/cards/mandarin-match-pinyin.tsx
+++ app/(language)/mandarin/quiz/cards/mandarin-match-pinyin.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 import { MatchCard } from "../../../quiz/match-cards";
 import { CardProps } from "../../../quiz/types/card";
 import { MandarinDefinition } from "../../api/mandarin";
-import { MandarinMatchPinyinCardParams } from "./card";
+import { MatchPinyinCardStat } from "../../api/stats";
+import { useMandarinQuizStatsStore } from "../store/useMandarinQuizStatsStore";
+import { MandarinCardType, MandarinMatchPinyinCardParams } from "./card";
 
 type MandarinMatchPinyinProps = CardProps<MandarinDefinition> &
   MandarinMatchPinyinCardParams;
@@ -15,6 +19,12 @@ export default function MandarinMatchPinyin({
   setRenderedResult,
   setOnAcknowledge,
 }: MandarinMatchPinyinProps) {
+  const [generatedTime] = useState(new Date());
+
+  const selectedPairs: [MandarinDefinition, MandarinDefinition][] = [];
+
+  const { addQuizCardStat } = useMandarinQuizStatsStore();
+
   const renderOption = (option: MandarinDefinition, isFrom: boolean) => {
     if (isFrom) {
       return <span className="text-2xl">{option.word}</span>;
@@ -23,13 +33,33 @@ export default function MandarinMatchPinyin({
     }
   };
 
+  const onMatched = (from: MandarinDefinition, to: MandarinDefinition) => {
+    selectedPairs.push([from, to]);
+  };
+
+  const onCompletedMatching = () => {
+    const stat: MatchPinyinCardStat = {
+      type: MandarinCardType.MatchPinyin,
+      generatedTime,
+      answeredTime: new Date(),
+      isCorrect: selectedPairs.length === options.length,
+      options: options.map((option) => option.word),
+      userSelections: selectedPairs.map((pair) => [pair[0].word, pair[1].word]),
+    };
+
+    addQuizCardStat(stat);
+
+    onAnswered();
+  };
+
   return (
     <MatchCard
       cardTitle="Match the Pinyin"
       options={options}
       renderOption={renderOption}
       quizState={quizState}
-      onAnswered={onAnswered}
+      onMatched={onMatched}
+      onAnswered={onCompletedMatching}
       onCorrect={onCorrect}
       onIncorrect={onIncorrect}
       setRenderedResult={setRenderedResult}
