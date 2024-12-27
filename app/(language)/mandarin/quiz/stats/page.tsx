b1d9a9ed4d9a41e8735a88512d7b151aa1b691b0
jumpcutfindo <danielhoe97@gmail.com>
2024-12-27 14:07:16 +0800

    Add general stats for mandarin quiz

--- app/(language)/mandarin/quiz/stats/page.tsx
+++ app/(language)/mandarin/quiz/stats/page.tsx
@@ -1,13 +1,91 @@
+"use client";
+
 import { LanguageBody, LanguageLayout } from "@/app/(language)/language-layout";
-import { faChartColumn } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarDays,
+  faChartColumn,
+  faCheckCircle,
+  faQuestionCircle,
+  faSun,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { MandarinLayoutHeader } from "../../mandarin-header";
+import { useMandarinQuizStatsStore } from "../store/useMandarinQuizStatsStore";
+import {
+  getLongestStreak,
+  getNumQuestionsAnswered,
+  getPercentageCorrect,
+  getQuestionsByDay,
+} from "./stat-processor";
+
+interface QuizStatTileProps {
+  title: string;
+  value: string;
+  icon: IconDefinition;
+}
+
+function QuizStatTile(props: QuizStatTileProps) {
+  const { title, value, icon } = props;
+
+  return (
+    <div className="flex flex-row border p-2 rounded-md justify-items-center items-center space-x-2">
+      <FontAwesomeIcon className="p-4 w-8 h-8" icon={icon} size="2xl" />
+      <div className="flex flex-col space-y-2">
+        <h2 className="text-3xl h-8">{value}</h2>
+        <p className="h-8">{title}</p>
+      </div>
+    </div>
+  );
+}
 
 export default function QuizStats() {
+  const { quizCardStats } = useMandarinQuizStatsStore();
+
+  const numQuestionsAnswered = getNumQuestionsAnswered(quizCardStats);
+  const percentageCorrect = getPercentageCorrect(quizCardStats);
+
+  const questionsByDay = getQuestionsByDay(quizCardStats);
+  const maxQuestionsInDay = Object.values(questionsByDay)
+    .map((questions) => questions.length)
+    .reduce((a, b) => Math.max(a, b), 0);
+
+  const longestStreak = getLongestStreak(quizCardStats);
+
   return (
     <LanguageLayout>
       <MandarinLayoutHeader headerIcon={faChartColumn} headerTitle="测验统计" />
-      <LanguageBody>This is a body</LanguageBody>
+      <LanguageBody>
+        <div className="flex flex-col w-full h-full">
+          <div className="flex flex-col w-full p-2 space-y-2">
+            <h1 className="my-auto text-sm font-bold">OVERALL</h1>
+            <div className="grid grid-cols-2 gap-2">
+              <QuizStatTile
+                title="questions"
+                value={numQuestionsAnswered.toString()}
+                icon={faQuestionCircle}
+              />
+              <QuizStatTile
+                title="correct"
+                value={`${percentageCorrect}%`}
+                icon={faCheckCircle}
+              />
+              <QuizStatTile
+                title="max qns in a day"
+                value={maxQuestionsInDay.toString()}
+                icon={faSun}
+              />
+
+              <QuizStatTile
+                title="longest streak"
+                value={longestStreak.toString()}
+                icon={faCalendarDays}
+              />
+            </div>
+          </div>
+        </div>
+      </LanguageBody>
     </LanguageLayout>
   );
 }
---
+++ app/(language)/mandarin/quiz/stats/stat-processor.ts
@@ -0,0 +1,57 @@
+import dayjs from "dayjs";
+
+import { QuizCardStat } from "../../api/stats";
+
+export function getNumQuestionsAnswered(quizCardStats: QuizCardStat[]) {
+  return quizCardStats.length;
+}
+
+export function getNumQuestionsCorrect(quizCardStats: QuizCardStat[]) {
+  return quizCardStats.filter((stat) => stat.isCorrect).length;
+}
+
+export function getPercentageCorrect(quizCardStats: QuizCardStat[]) {
+  return (
+    (getNumQuestionsCorrect(quizCardStats) /
+      getNumQuestionsAnswered(quizCardStats)) *
+    100
+  );
+}
+
+export function getQuestionsByDay(quizCardStats: QuizCardStat[]) {
+  const questionsByDay: Record<string, QuizCardStat[]> = {};
+
+  for (const stat of quizCardStats) {
+    const date = new Date(stat.answeredTime).toDateString();
+
+    if (!questionsByDay[date]) {
+      questionsByDay[date] = [];
+    }
+
+    questionsByDay[date].push(stat);
+  }
+
+  return questionsByDay;
+}
+
+export function getLongestStreak(quizCardStats: QuizCardStat[]) {
+  const questionsByDay = getQuestionsByDay(quizCardStats);
+
+  const dates = Object.keys(questionsByDay).sort((a, b) =>
+    dayjs(a).isAfter(b) ? 1 : -1,
+  );
+
+  let longestStreak = 1,
+    currStreak = 1;
+  for (let i = 1; i < dates.length; i++) {
+    const date = dates[i];
+    if (dayjs(date).diff(dayjs(dates[i - 1]), "day") === 1) {
+      currStreak++;
+      longestStreak = Math.max(longestStreak, currStreak);
+    }
+  }
+
+  console.log(longestStreak);
+
+  return longestStreak;
+}
